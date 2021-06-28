b1a4d488e2aafd50ba6f16905447b45672f4868d
Syuzanna Simonyan <syuzanna.simonyan@wic.local>
2021-06-28 14:45:40 +0400

    added files

--- ageDecider.js
+++ ageDecider.js
@@ -1,13 +1,18 @@
 function findAge(yearOfBirth) {
 
 
-	let now = new Date();
-	let year = now.getFullYear();
+	let now = Date.now();
 	
-	console.log(year - yearOfBirth);
+	let past = new Date(yearOfBirth).getTime();
+
+    console.log(past);
+	
+	console.log(Math.floor((now-past) /(3600*1000*24*365)));
+	
 	
 }
 
-findAge('1945');
+
+findAge('1994-06-15');
 
 
